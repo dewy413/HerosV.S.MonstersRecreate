9dfbeca28f948b96e007f6c54f113d30ab7eface
Jordan Deuley <114099341+dewy413@users.noreply.github.com>
2023-06-01 15:34:18 -0500

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include "raylib.h"
+
+using namespace std;
+
+
+Vector2 checkLocation(Vector2 current, Vector2 goTo) {
+    if(current.x != goTo.x) {
+        current.x = goTo.x;
+    }
+
+    if(current.y != goTo.y) {
+        current.y = goTo.y;
+    }
+
+    return current;
+
+}
+
+int main() {
+
+    const int screenWidth = 800;
+    const int screenHeight = 450;
+
+    InitWindow(screenWidth, screenHeight, "HVM");
+
+    SetTargetFPS(60);
+
+
+
+    Vector2 goToLocation = GetMousePosition(); // Gets the mouse XY
+    Vector2 boxLocation = {0, 0};
+
+
+
+
+
+    while (!WindowShouldClose()) { // Variable Update Zone
+
+        boxLocation = checkLocation(goToLocation, boxLocation); // Should update the mouse to be at the location of where the
+        BeginDrawing(); //Start the Drawing
+
+            ClearBackground(RAYWHITE);
+            DrawRectangle(boxLocation.x, boxLocation.y, 80, 80, RED); // Creates the box
+        EndDrawing();
+
+    }
+
+
+    return 0;
+}
