dd0227d8b0f4ec8abdb043d31e56349ff06130d2
Jordan Deuley <114099341+dewy413@users.noreply.github.com>
2023-06-20 09:13:26 -0500

    Fixed some little code things.

--- main.cpp
+++ main.cpp
@@ -8,13 +8,23 @@ using namespace std;
 
 class entity {
 public:
-    //Rectangle hitBox;
-    Vector2 boxLocation = {0, 0};
-    Vector2 goToLocation = {0, 0};
+    string name;
+    Vector2 boxLocation;
+    Vector2 goToLocation;
+    Rectangle hitBox;
     int speed = 5;
 
-
+    explicit entity(Vector2 location) {
+        boxLocation = location;
+        goToLocation = boxLocation;
+        hitBox.x = location.x;
+        hitBox.y = location.y;
+        hitBox.height = 50;
+        hitBox.width = 50;
+    }
     void locationCheck() {
+        hitBox.x = boxLocation.x;
+        hitBox.y = boxLocation.y;
         if(!checkLocation()) {
             updateLocation();
         }
@@ -37,7 +47,7 @@ public:
         if(boxLocation.x > int(goToLocation.x)) {
             boxLocation.x -= speed;
         }
-        if(boxLocation.x < int(goToLocation.x)) {
+        if(boxLocation.x< int(goToLocation.x)) {
             boxLocation.x += speed;
         }
         if(boxLocation.y > int(goToLocation.y)) {
@@ -49,7 +59,9 @@ public:
         }
 
     }
-
+    void drawSelf(Color color) const {
+        DrawRectangleRec(hitBox, color);
+    }
 
 };
 
@@ -68,16 +80,15 @@ int main() {
 
 
 
-
     Vector2 *selectedLocation;
     Vector2 *goToLocation;
     Vector2 mouseLocation;
     vector <entity> entities;
     entity *selected;
-    entity boxOne;
-    entity boxTwo;
-    entity boxThree;
-    entity boxFour;
+    entity boxOne({0, 0});
+    entity boxTwo({50, 50});
+    entity boxThree({100, 100});
+    entity boxFour({150, 150});
 
     entities.push_back(boxOne);
     entities.push_back(boxTwo);
@@ -85,20 +96,6 @@ int main() {
     entities.push_back(boxFour);
 
 
-    boxOne.boxLocation = {0, 0};
-    //boxOne.hitBox = {boxOne.boxLocation.x, boxOne.boxLocation.y, 50, 50};
-
-    boxTwo.boxLocation = {50, 50};
-    //boxTwo.hitBox = {boxTwo.boxLocation.x, boxTwo.boxLocation.y, 50, 50};
-    boxTwo.goToLocation = boxTwo.boxLocation;
-
-    boxThree.boxLocation = {100, 100};
-    //boxThree.hitBox = {boxThree.boxLocation.x, boxThree.boxLocation.y, 50, 50};
-    boxThree.goToLocation = boxThree.boxLocation;
-
-    boxFour.boxLocation = {150, 150};
-    //boxFour.hitBox = {boxFour.boxLocation.x, boxFour.boxLocation.y, 50, 50};
-    boxFour.goToLocation = boxFour.boxLocation;
 
     selectedLocation = &boxOne.boxLocation;
     goToLocation = &boxOne.goToLocation;
@@ -165,10 +162,10 @@ int main() {
 
         BeginDrawing(); //Start the Drawing
         ClearBackground(BLACK);
-            DrawRectangle(boxOne.boxLocation.x, boxOne.boxLocation.y, 40, 40, RED); // Creates the box
-            DrawRectangle(boxTwo.boxLocation.x, boxTwo.boxLocation.y, 40, 40, BLUE);
-            DrawRectangle(boxThree.boxLocation.x, boxThree.boxLocation.y, 40, 40, YELLOW);
-            DrawRectangle(boxFour.boxLocation.x, boxFour.boxLocation.y, 40, 40, GREEN);
+            boxOne.drawSelf(RED);
+            boxTwo.drawSelf(BLUE);
+            boxThree.drawSelf(GREEN);
+            boxFour.drawSelf(PURPLE);
         EndDrawing();
 
     }
